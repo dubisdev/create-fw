2b63ac2b8320004df51b396dd2dddd3373a21fae
dubisdev <dubisdev@gmail.com>
2021-10-23 20:40:12 +0200

    feat: add user outputs and spinners
    Closes #15
    
    re #15

--- src/templates/jest.ts
+++ src/templates/jest.ts
@@ -1,28 +1,42 @@
 import execa from "execa";
 import { getPkgManager } from "../lib/arg.js";
 import basic from "./basic.js";
+import ora from "ora";
+import chalk from "chalk";
 
 const pkgManager = await getPkgManager();
 
 export default async () => {
 	await basic(); //generates package.json
 
+	// install jest
+
 	await installJest();
+
 	await createJestConfigFile();
 };
 
 const installJest = () => {
-	return execa.command(
-		`${pkgManager} ${pkgManager !== "yarn" ? "i" : "add"} jest -D`,
-		{ stdio: "inherit" }
-	);
+	let installSpinner = ora("Downloading Jest").start();
+	return execa
+		.command(`${pkgManager} ${pkgManager !== "yarn" ? "i" : "add"} jest -D`)
+		.then(() => {
+			installSpinner.stop();
+			console.log(chalk.bold("🪓 Installed: Jest"));
+		});
 };
 
 const createJestConfigFile = () => {
 	switch (pkgManager) {
 		case "yarn":
-			return execa.command(`yarn jest --init`, { stdio: "inherit" });
+			return execa
+				.command(`yarn jest --init`, { stdio: "inherit" })
+				.then(() => {
+					console.log(chalk.bold("\n🪓 Generated: jest-config file"));
+				});
 		default:
-			return execa.command(`npx jest --init`, { stdio: "inherit" });
+			return execa.command(`npx jest --init`, { stdio: "inherit" }).then(() => {
+				console.log(chalk.bold("\n🪓 Generated: jest-config file"));
+			});
 	}
 };
